41d718608daff04dc348a1ea4bf46cc1542a4b98
Jashcraf <jashcraft@arizona.edu>
2025-01-24 11:28:50 -1000

    pushing updates for speckle nulling

--- poi/efc.py
+++ poi/efc.py
@@ -1,18 +1,15 @@
 
 
-class iEFC:
+class ADEFC:
 
-    def __init__(self, forward, DM, calibration_modes, dark_hole):
+    def __init__(self, DM, calibration_modes, dark_hole):
 
-        self.forward = forward
         self.DM = DM
         self.calibration_modes = calibration_modes
         self.dark_hole = dark_hole
 
+    def forward()
 
-    def build_jacobian(self, verbose=False):
-        pass
 
-    def step():
-        pass
+    
 
