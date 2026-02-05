I think I broke the MSE cost function - solutions not converging well anymore. Need to figure out what's wrong in the gradient

poi/aplc_design.py
@@ -212,11 +212,14 @@ class BaseAPLC:
             shift=(-self.shiftx, 0),
             method='mdft')
 
-        I = np.abs(D)**2
-        N = I / self.contrast_norm
+        #I = np.abs(D)**2
+        #N = I / self.contrast_norm
+        
+        # Update the contrast normalization
+        self.cost_function.norm = self.contrast_norm
         
         # Evaluate cost function
-        E = self.cost_function.forward(N[self.dh])
+        E = self.cost_function.forward(D[self.dh])
         E *= self.weight
 
         def plot_x():
@@ -242,8 +245,8 @@ class BaseAPLC:
         # ipdb.set_trace()
         # print(f"Intermediate Cost = {E}")
 
-        self.I = I
-        self.N = N
+        self.I = self.cost_function.I
+        self.N = self.cost_function.N
         self.E = E
         self.cost.append(self.E)
         
@@ -266,14 +269,11 @@ class BaseAPLC:
         self.update(x)
         
         # Backpropagate image intensity to image field gradient
-        Nbar = np.zeros(self.dh.shape, dtype=np.float64)
+        Dbar = np.zeros(self.dh.shape, dtype=np.complex128)
 
         # Backpropagation of cost function might be erroneous
-        Nbar[self.dh] = self.cost_function.reverse(self.N[self.dh])
-        Nbar *= self.weight
-
-        Ibar = Nbar / self.contrast_norm
-        Dbar = 2 * Ibar * self.D
+        Dbar[self.dh] = self.cost_function.reverse(self.D[self.dh])
+        Dbar *= self.weight
 
         # backprop from image to lyot stop
         dbar = focus_fixed_sampling_backprop(
@@ -285,7 +285,6 @@ class BaseAPLC:
             output_samples=self.aplc.shape,
             shift=(0, 0),
             method='mdft')
-
         # backprop lyot stop application - conjugate permits complex ls
         cbar = self.ls.conj() * dbar
 
@@ -296,7 +295,7 @@ class BaseAPLC:
             prop_dist = self.efl,
             wavelength=self.wvl,
             output_dx=self.amp_dx,
-            output_samples=self.I.shape, 
+            output_samples=self.dh.shape, 
             shift=(self.shiftx, 0),
             method='mdft')
 
@@ -326,13 +325,13 @@ class BaseAPLC:
             abar = np.tensordot(self.basis, aplcbar)
         
         # The intermediate gradients
-        self.Ibar = Ibar
         self.Bbar = Bbar
         self.cbar = cbar
         self.Cbar = Cbar
         self.dbar = dbar
         self.Dbar = Dbar
-        self.Nbar = Nbar
+        self.Ibar = self.cost_function.Ibar
+        self.Nbar = self.cost_function.Nbar
 
         if not self.zonal:
             self.abar = abar
@@ -496,19 +495,19 @@ class BaseSPC:
         # Get contrast normalization (approx)
         self.contrast_norm = (np.abs(B)**2).max()
 
-        I = np.abs(B)**2
-        N = I / self.contrast_norm
+        #I = np.abs(B)**2
+        #N = I / self.contrast_norm
         
         # Evaluate cost function
         # Multiplying is a little weird here because MSE gets 
         # kind of thrown off. But this makes it compatible
         # With core throughput optimization as well. Alternatively,
         # We could have self.dh = the core mask?
-        E = self.cost_function.forward(N[self.dh])
+        E = self.cost_function.forward(B[self.dh])
         E *= self.weight
 
-        self.I = I
-        self.N = N
+        self.I = self.cost_function.I
+        self.N = self.cost_function.N
         self.E = E
         self.cost.append(self.E)
 
@@ -528,9 +527,7 @@ class BaseSPC:
         
         # Backpropagate image intensity to image field gradient
         Nbar = np.zeros(self.dh.shape, dtype=np.float64)
-        Nbar[self.dh] = self.cost_function.reverse(self.N[self.dh])
-        Ibar = Nbar / self.contrast_norm * self.weight
-        Bbar = 2 * Ibar * self.B
+        Bbar[self.dh] = self.cost_function.reverse(self.x[self.dh])
 
         # backprop from before fpm to pupil apodizer
         self.bbar = focus_fixed_sampling_backprop(

poi/cost_functions.py
@@ -38,38 +38,128 @@ def softmax(x, alpha=1):
 
 
 class LogSumExp:
-    def __init__(self, target=0, alpha=1):
+    def __init__(self, target=0., alpha=1., norm=1.):
         """Object interface for the LogSumExp cost function, with
         'forward' and 'reverse' method for use in models with 
         analytic gradients.
+
+        Notes
+        -----
+        Computes the LogSumExp of an intensity quantity, but the
+        input is the complex electric field
+
+        Parameters
+        ----------
+        target: float
+            Desired value for this function to take in optimization
+        alpha: float
+            Steepness parameter used to control how closely this
+            function approximates max(). This can be thought of
+            as the slope.
+        norm: float
+            Value to normalize the computed intensity by. This can
+            also be modified `in place` by setting Class.norm = value
+            
         """
         self.target = target
         self.alpha = alpha
+        self.norm = norm
 
     def forward(self, x):
-        return log_sum_exp(self.target - x, alpha=self.alpha)
+        """
+        Parameters
+        ----------
+        x: ndarray
+            Complex e-field quantities that some operation will be
+            performed on.
+
+        Returns
+        -------
+        float
+            Value of the cost function
+        """
+        self.I = np.abs(x) ** 2
+        self.I /= self.norm
+        return log_sum_exp(self.target - self.I, alpha=self.alpha)
 
     def reverse(self, x):
-        return softmax(self.target - x, alpha=self.alpha) 
+        """
+        Parameters
+        ----------
+        x: ndarray
+            Real-valued gradient w.r.t. the cost function
+
+        Returns
+        -------
+        ndarray
+            Gradient of the cost function w.r.t. the intensity
+        """
+        upstream = softmax(self.target - x, alpha=self.alpha)
+        upstream /= self.norm
+        Ibar = 2 * self.I * upstream
+        return Ibar 
 
 
 class MaxContrast:
-    def __init__(self, target=0, alpha=1):
+    def __init__(self, target=0, alpha=1, norm=1.):
         """
         Targets maximum value, uses softmax to approximate gradient
+        of the maximum. May lead to instability due to the rapid variation
+        of the np.max() function.
+        
+        Parameters
+        ----------
+        target: float
+            Desired value for this function to take in optimization
+        alpha: float
+            Steepness parameter used to control how closely this
+            function approximates max(). This can be thought of
+            as the slope.
+        norm: float
+            Value to normalize the computed intensity by. This can
+            also be modified `in place` by setting Class.norm = value
         """
         self.target = target
         self.alpha = alpha
+        self.norm = norm
 
     def forward(self, x):
-        return np.max(self.target - x)
+        """
+        Parameters
+        ----------
+        x: ndarray
+            Complex e-field quantities that some operation will be
+            performed on.
+
+        Returns
+        -------
+        float
+            Value of the cost function
+        """
+        self.I = np.abs(x) ** 2
+        self.I /= self.norm
+        return np.max(self.target - self.I)
 
     def reverse(self, x):
-        return softmax(self.target - x, alpha=self.alpha)
+        """
+        Parameters
+        ----------
+        x: ndarray
+            Real-valued gradient w.r.t. the cost function
+
+        Returns
+        -------
+        ndarray
+            Gradient of the cost function w.r.t. the intensity
+        """
+        upstream = softmax(self.target - x, alpha=self.alpha)
+        upstream /= self.norm
+        Ibar = 2 * self.I * upstream
+        return Ibar 
 
 
 class MeanSquaredErrorLinear:
-    def __init__(self, target=0, alpha=1.):
+    def __init__(self, target=0., alpha=1., norm=1.):
         """Mean squared error cost function with a linear
         penalty. Sign changes when constraint target is satisfied.
         In english, this means you are asking the optimizer:
@@ -83,23 +173,53 @@ class MeanSquaredErrorLinear:
         alpha: float
             Weight to multiply constraint by. Can be negative
             to flip the sign convention
+        norm: float
+            Value to normalize the computed intensity by. This can
+            also be modified `in place` by setting Class.norm = value
         """
         self.target = target ** 2
         self.alpha = alpha
+        self.norm = norm
 
     def forward(self, x):
-        err = x
-        mse_mag = np.mean(err**2)
+        """
+        Parameters
+        ----------
+        x: ndarray
+            Complex e-field quantities that some operation will be
+            performed on.
+
+        Returns
+        -------
+        float
+            Value of the cost function
+        """
+        self.I = np.abs(x) ** 2
+        self.N = self.I / self.norm
+        err = self.N
+        self.mse_mag = np.mean(err**2)
         return (mse_mag - self.target) * self.alpha
 
     def reverse(self, x):
-        err = x 
-        mse_grad = 2 * err / x.size 
-        return mse_grad * self.alpha 
+        """
+        Parameters
+        ----------
+        x: ndarray
+            Real-valued gradient w.r.t. the cost function
+
+        Returns
+        -------
+        ndarray
+            Gradient of the cost function w.r.t. the intensity
+        """
+        self.Nbar = 2 * self.mse_mag * alpha / x.size
+        self.Ibar = self.Nbar / self.norm
+        Ebar = 2 * x * self.Ibar
+        return Ebar 
 
 
 class MeanSquaredErrorQuadratic:
-    def __init__(self, target=0, alpha=1.):
+    def __init__(self, target=0, alpha=1., norm=1.):
         """Mean squared error cost function with a linear
         penalty. Sign changes when constraint target is satisfied
         In english, this means you are asking the optimizer:
@@ -112,19 +232,51 @@ class MeanSquaredErrorQuadratic:
         alpha: float
             Weight to multiply constraint by. Can be negative
             to flip the sign convention
+        norm: float
+            Value to normalize the computed intensity by. This can
+            also be modified `in place` by setting Class.norm = value
         """
         self.target = target
         self.alpha = alpha
+        self.norm = norm
 
     def forward(self, x):
-        err = x - self.target
-        mse_mag = np.sum(err**2)
-        return mse_mag * self.alpha
+        """
+        Parameters
+        ----------
+        x: ndarray
+            Complex e-field quantities that some operation will be
+            performed on.
+
+        Returns
+        -------
+        float
+            Value of the cost function
+        """
+        self.x = x
+        self.I = np.abs(self.x) ** 2
+        self.N = self.I / self.norm
+        err = self.N - self.target
+        self.mse_mag = np.sum(err**2)
+        return self.mse_mag * self.alpha
 
     def reverse(self, x):
-        err = x - self.target 
-        mse_grad = 2 * err#  / x.size 
-        return mse_grad * self.alpha 
+        """
+        Parameters
+        ----------
+        x: ndarray
+            Real-valued gradient w.r.t. the cost function
+
+        Returns
+        -------
+        ndarray
+            Gradient of the cost function w.r.t. the intensity
+        """
+        self.msebar = 2 * self.mse_mag * self.alpha
+        self.Nbar = self.msebar
+        self.Ibar = self.Nbar / self.norm
+        Ebar = 2 * self.x * self.Ibar  
+        return Ebar
 
 class PNorm:
     def __init__(self, target=0, alpha=10):

scripts/test_point_symmetry.py
@@ -40,18 +40,18 @@ OWA = 20
 AZMIN = -65 / 2 # Defines the angular extend of the dark zone
 AZMAX = 65 / 2
 BANDWIDTH = 10 # percent
-NWVLS = 3
+NWVLS = 5
 OVERSAMPLE = 4 # pix per lam/D
 pth_to_aperture = Path.home() / "poi/hex_pupil_amplitude_6510mm_1024pix.fits"
 pth_to_aperture = Path.home() / "poi/luvoir_b_pupil_512.0_shift_px_py.fits"
-LS_FRAC = 0.9 # Fraction of the pupil radius to use for the Lyot stop
+LS_FRAC = 0.95 # Fraction of the pupil radius to use for the Lyot stop
 LS_OBSCURATION_RATIO = 0.0 # Ratio of the Lyot stop obscuration to the pupil radius
-MAX_ITERS = 100_000
+MAX_ITERS = 10000
 core_size = 0.7 # radius in lam/D
 TARGET_CONTRAST = 1e-11
 
 CONTRAST_RELATIVE_WEIGHT = 1 # 1e10 worked here, 1e7 too low for point-symmetric
-THROUGHPUT_RELATIVE_WEIGHT =  1e-20
+THROUGHPUT_RELATIVE_WEIGHT =  10 ** (-1 * 20.3)#1e-20
 #       Binary Contrast
 # 1e-10  [~]     [x]
 #  *1e2  [~]     [x]
@@ -191,7 +191,8 @@ _, _ = opt_contrast_throughput.fg(x0)
 plt.figure()
 plt.subplot(131)
 plt.title("Coro PSF before optimization")
-plt.imshow(opt_contrast_throughput.optlist[0].I.get(), cmap="inferno", norm=LogNorm())
+I = np.abs(opt_contrast_throughput.optlist[0].D).get()
+plt.imshow(I, cmap="inferno", norm=LogNorm())
 plt.colorbar()
 plt.subplot(132)
 plt.title("Re|Pupil Gradient| before optimization")
@@ -216,33 +217,6 @@ focal_knife_left = np.fliplr(focal_knife_right)
 
 B_right = np.abs(_opt.B)**2 * focal_knife_right
 B_left = np.fliplr(np.abs(_opt.B)**2 * focal_knife_left)
-
-plt.figure()
-plt.subplot(241)
-plt.imshow((b_right - b_left).get() / _opt.amp_select.get(), cmap="Spectral")
-plt.colorbar()
-plt.subplot(242)
-plt.imshow((B_right - B_left).get() / focal_knife_right.get(), cmap="Spectral")
-plt.colorbar()
-plt.subplot(243)
-plt.imshow(np.real(_opt.c * ls_mask).get())
-plt.colorbar()
-plt.subplot(244)
-plt.imshow(tnp.abs(_opt.I.get())**2, norm=LogNorm())
-plt.colorbar()
-plt.subplot(245)
-plt.imshow(np.real(_opt.bbar).get())
-plt.colorbar()
-plt.subplot(246)
-plt.imshow(tnp.abs(_opt.Cbar.get())**2, norm=LogNorm())
-plt.colorbar()
-plt.subplot(247)
-plt.imshow(np.real(_opt.cbar).get())
-plt.colorbar()
-plt.subplot(248)
-plt.imshow(tnp.abs(_opt.Ibar.get())**2, norm=LogNorm())
-plt.colorbar()
-plt.show()
 
 # initialize the optimizer with box constraints
 opt = F77LBFGSB(opt_contrast_throughput.fg, x0,